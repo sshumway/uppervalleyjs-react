27abe590305945c6fe89836ec68395492bbfe582
Scott Shumway <shumwaysr@gmail.com>
2017-08-07 13:36:31 -0400

    removed firebase config values

--- react-demo/src/fire.js
+++ react-demo/src/fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyC0MFq5nmgs1rSOnQyWyb-iPLcxjzHQ8qA",
-  authDomain: "uppervalleyjs-react-demo-app.firebaseapp.com",
-  databaseURL: "https://uppervalleyjs-react-demo-app.firebaseio.com",
-  projectId: "uppervalleyjs-react-demo-app",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "106446678676"
+  messagingSenderId: ""
 };
 
 var fire = firebase.initializeApp(config);
